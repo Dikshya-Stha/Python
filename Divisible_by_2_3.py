286a33aafe258013ca4f5e0713e2b0dfc4c27d05
dikshyaaaaaa <dikstha1409@gmail.com>
2024-03-20 20:53:19 +0545

    innital commit

---
+++ Divisible_by_2_3.py
@@ -0,0 +1,4 @@
+a = [ 2, 3, 6, 8, 9,12,15, 18, 24, 22, 33, 112]
+for num in a:
+    if num%2 == 0 and num%3 ==0:
+        print(num)
---
+++ Even_position.py
@@ -0,0 +1,5 @@
+a = [43, 23, 21, 44, 56, 75]
+even_position = []
+for i in range(1, len(a),2):
+    even_position.append(a[i])
+print(even_position)    
